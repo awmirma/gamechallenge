62702397c493ab12677098ddacf454f0ce1593b0
amir <amirarabi3131@gmail.com>
2021-05-29 00:28:09 +0430

    some changes

--- GEMBALL-filecomposition/movements.h
+++ GEMBALL-filecomposition/movements.h
@@ -1,32 +1,33 @@
 
 
+
 #ifndef movements_h
 #define movements_h
 #include <iostream>
 using namespace std ;
 
-class team ;
-class movements
+class team ; // each team in the game
+class movements // movements of the game including pass , dribble , tackle , shoot and save
 {
     friend class team ;
-    friend istream &operator >> (istream &input , team &player_team ) ;
-    friend bool pass (team player[2],int first, int second);
-    friend bool dribble (team player[2],int first, int second);
-    friend bool shoot (team player[2],int first, int second) ;
+    friend istream &operator >> (istream &input , team &player_team ) ; // to enter the team that the player tends to make ( the team 's name and its authority )
+    friend bool pass_and_tackle (team player[2],int first, int second); // used for pass and tackle
+    friend bool dribble_and_tackle (team player[2],int first, int second); // used for dribble and tackle
+    friend bool shoot_and_save (team player[2],int first, int second) ; // used for shoot and save
     public :
     
-    void default_accuracy (int );
+    void default_accuracy (int ); // the accuracy of the team made by player
     
     private :
     
-    // accuracy
+    // accuracy    // probability of the movements veracity ( per 100 )
     int shoot_accuracy ;
     int pass_accuracy ;
     int tackle_accuracy ;
     int dribble_accuracy ;
     int save_accuracy ;
     
-    // GEM
+    // GEM     // the score that is substracted by the each movement
     int shoot_GEM = 3 ;
     int pass_GEM = 1 ;
     int tackle_GEM = 2 ;
