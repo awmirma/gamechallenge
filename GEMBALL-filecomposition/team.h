df26efec10786cb2b0a7ce4294601c155e44b945
Javad <74994427+javadfaraji99@users.noreply.github.com>
2021-05-28 22:10:07 +0430

    Update team.h

--- GEMBALL-filecomposition/team.h
+++ GEMBALL-filecomposition/team.h
@@ -6,35 +6,35 @@
 using namespace std ;
 
 
-class team
+class team // each team in the game
 {
-    friend istream &operator >> (istream &inputt , team &player_team ) ;
-    friend class movements ;
+    friend istream &operator >> (istream &inputt , team &player_team ) ;// to enter the team that the player tends to make ( the team 's name and its authority )
+    friend class movements ; // movements of the game including pass , dribble , tackle , shoot and save
     friend void selecting_team (team player_team) ;
-    friend bool pass (team player[2],int first, int second);
-    friend bool dribble (team player[2],int first, int second);
-    friend bool shoot (team player[2],int first, int second) ;
+    friend bool pass_and_tackle (team player[2],int first, int second); // used for pass and tackle
+    friend bool dribble_and_tackle (team player[2],int first, int second); // used for dribble and tackle
+    friend bool shoot_and_save (team player[2],int first, int second) ; // used for shoot and save
     public :
     
     team ();// constructor
     
-    void complete (int );
+    void complete (int ); // sets each team with its attributes' accuracy
     
-    void add_gems(int );
+    void add_gems(int ); // adding gems in the rounds
     
-    string name_getter(){return name;}
+    string name_getter(){return name;} // shows the team name
     
-    int goal_getter(){return goals;}
+    int goal_getter(){return goals;} // shows the goals that each team has scored
     
-    int GEM_getter(){return GEMS;}
+    int GEM_getter(){return GEMS;} // shows the gems that each team has 
     
-    void goal_setter(){goals++;}
+    void goal_setter(){goals++;} // setting the goal that is being scored
     
     private :
     
     string name  ;
-    movements move ;
-    int GEMS = 0 ;
+    movements move ; // movements of each team
+    int GEMS = 0 ; 
     int goals = 0 ;
     
 };
